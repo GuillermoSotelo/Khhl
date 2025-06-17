4691647bbcbbbb8df537f01dad3b793ebc8e6ea8
GuillermoSotelo <90469245+GuillermoSotelo@users.noreply.github.com>
2025-06-17 14:48:28 -0300

    Update tp 2.py

--- tp 2.py
+++ tp 2.py
@@ -37,13 +37,14 @@ def principal():
     line=texto.readline()
     cant_binvalida=0
     cant_minvalida=0
+    cont_valido=0
     condicion_minvalida1=False
     condicion_minvalida2=False
     for line in texto:
-        nombre_destinatario=line[0:20]
+        nombre_destinatario=line[0:20].strip()
         codigo_identificacion=line[20:30].strip()
-        orden_de_pago=line[30:40]
-        monto_nominal=line[40:50]
+        orden_de_pago=line[30:40].strip()
+        monto_nominal=line[40:50].strip()
         ide_algoritmo_comision=line[50:52]
         ide_algoritmo_impositvo=line[52:54]
         
@@ -53,41 +54,35 @@ def principal():
              
              if not es_guion(c):
                   todos_guiones=False
-            
-             
-          #    if (not es_mayuscula(c) and es_numero(c) and not es_guion(c)):#todos numeros(valido)
-          #         codigo_identificacion_destinatario_es_invalido=False
-          #         print("hola mundo")
-          #    if (es_mayuscula(c) and not es_numero(c) and not es_guion(c)):#todas mayusculas(valido)
-          #         codigo_identificacion_destinatario_es_invalido=False
-          #    if (es_mayuscula(c) or es_numero(c)) and (not es_guion(c)):#mayusculas y numeros(sin guiones, valido)
-          #         codigo_identificacion_destinatario_es_invalido=False
              if (not es_mayuscula(c) and not es_numero(c) and not es_guion(c)):
                     codigo_identificacion_destinatario_es_invalido=True
                
-          
         condicion_minvalida1,condicion_minvalida2=False,False     
         if (not codigo_moneda(orden_de_pago) or not dos_o_mas_diferentes(orden_de_pago)):
              condicion_minvalida1=True
+             
         if codigo_identificacion_destinatario_es_invalido or todos_guiones:
              condicion_minvalida2=True
-              
+        
+
         
         
+        valido=True
         if condicion_minvalida1 or (condicion_minvalida1 and condicion_minvalida2):
              cant_minvalida+=1
-        if condicion_minvalida2 and (codigo_moneda(orden_de_pago) and dos_o_mas_diferentes(orden_de_pago)):
-            cant_binvalida+=1
-     #    if not (condicion_minvalida1 and condicion_minvalida2) and condicion_minvalida2:
-     #         cant_binvalida+=1
+             valido=False
+        if not (condicion_minvalida1 and condicion_minvalida2) and condicion_minvalida2:
+             cant_binvalida+=1
+             valido=False
+        if valido:
+             cont_valido+=1
+        
     print("cont minvalida",cant_minvalida)
     print("cont_binvalida",cant_binvalida)
+    print("cont_valido", cont_valido)
         
 
 
         
 
 principal()
-   
-
-   
