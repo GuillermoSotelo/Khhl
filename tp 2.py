b8956e0d668f0a6f7c6dd15429d35edbc5f57d10
GuillermoSotelo <90469245+GuillermoSotelo@users.noreply.github.com>
2025-06-17 14:10:53 -0300

    Update tp 2.py

--- tp 2.py
+++ tp 2.py
@@ -12,7 +12,7 @@ def dos_o_mas_diferentes(moneda):
     
     if "ARS" in moneda:
          x+=1
-    if "USD" in moneda:#si "USD" in "USDUSD"
+    if "USD" in moneda:
          x+=1
     if "EUR" in moneda:
          x+=1
@@ -37,6 +37,8 @@ def principal():
     line=texto.readline()
     cant_binvalida=0
     cant_minvalida=0
+    condicion_minvalida1=False
+    condicion_minvalida2=False
     for line in texto:
         nombre_destinatario=line[0:20]
         codigo_identificacion=line[20:30].strip()
@@ -44,8 +46,7 @@ def principal():
         monto_nominal=line[40:50]
         ide_algoritmo_comision=line[50:52]
         ide_algoritmo_impositvo=line[52:54]
-        if not codigo_moneda(orden_de_pago) or not dos_o_mas_diferentes(orden_de_pago):
-             cant_minvalida+=1
+        
         codigo_identificacion_destinatario_es_invalido=False
         todos_guiones=True
         for c in codigo_identificacion:
@@ -53,18 +54,40 @@ def principal():
              if not es_guion(c):
                   todos_guiones=False
             
-             if not (es_mayuscula(c) or es_numero(c) or es_guion(c)):
-                  codigo_identificacion_destinatario_es_invalido=True
+             
+          #    if (not es_mayuscula(c) and es_numero(c) and not es_guion(c)):#todos numeros(valido)
+          #         codigo_identificacion_destinatario_es_invalido=False
+          #         print("hola mundo")
+          #    if (es_mayuscula(c) and not es_numero(c) and not es_guion(c)):#todas mayusculas(valido)
+          #         codigo_identificacion_destinatario_es_invalido=False
+          #    if (es_mayuscula(c) or es_numero(c)) and (not es_guion(c)):#mayusculas y numeros(sin guiones, valido)
+          #         codigo_identificacion_destinatario_es_invalido=False
+             if (not es_mayuscula(c) and not es_numero(c) and not es_guion(c)):
+                    codigo_identificacion_destinatario_es_invalido=True
+               
+          
+        condicion_minvalida1,condicion_minvalida2=False,False     
+        if (not codigo_moneda(orden_de_pago) or not dos_o_mas_diferentes(orden_de_pago)):
+             condicion_minvalida1=True
         if codigo_identificacion_destinatario_es_invalido or todos_guiones:
-            cant_binvalida+=1
-        if not codigo_moneda(orden_de_pago) and not dos_o_mas_diferentes(orden_de_pago) and codigo_identificacion_destinatario_es_invalido:
+             condicion_minvalida2=True
+              
+        
+        
+        if condicion_minvalida1 or (condicion_minvalida1 and condicion_minvalida2):
              cant_minvalida+=1
-            
-    print(cant_minvalida,cant_binvalida)
+        if condicion_minvalida2 and (codigo_moneda(orden_de_pago) and dos_o_mas_diferentes(orden_de_pago)):
+            cant_binvalida+=1
+     #    if not (condicion_minvalida1 and condicion_minvalida2) and condicion_minvalida2:
+     #         cant_binvalida+=1
+    print("cont minvalida",cant_minvalida)
+    print("cont_binvalida",cant_binvalida)
         
 
 
         
 
 principal()
+   
+
    
